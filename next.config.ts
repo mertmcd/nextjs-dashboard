8ba7bca126850b7690b6440117052b442fe32d92
mertmcd <merttdem@gmail.com>
2025-03-06 20:55:17 +0300

    Remove experimental PPR configuration from layout and next.config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  experimental: {
-    ppr: "incremental",
-  },
 };
 
 export default nextConfig;
